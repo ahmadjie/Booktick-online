a86d33aa33a182dc5364830dc718b00ddc37ee08
ahmadjie <ahmad.adjiep@gmail.com>
2020-01-25 13:58:23 +0700

    deploy

---
+++ build/precache-manifest.f1caaa0f7e28bdec9b471569a41d2fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11334bea3f2416d82b0d40c15243f0e",
+    "url": "/Booktick-Production/index.html"
+  },
+  {
+    "revision": "683a4741df74b2d393ca",
+    "url": "/Booktick-Production/static/css/main.d01dcbfb.chunk.css"
+  },
+  {
+    "revision": "575d26b02cb327ec3451",
+    "url": "/Booktick-Production/static/js/2.eed5e3d8.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/Booktick-Production/static/js/2.eed5e3d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "683a4741df74b2d393ca",
+    "url": "/Booktick-Production/static/js/main.0ac72b23.chunk.js"
+  },
+  {
+    "revision": "f98535dae31131b34cf3",
+    "url": "/Booktick-Production/static/js/runtime-main.1fb4d68e.js"
+  }
+]);
